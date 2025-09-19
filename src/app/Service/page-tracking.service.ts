51ace25ad05fd5f6f297d3724ebbd0b1c6114610
Alex G <AlexGuerraTE@gmail.com>
2025-09-19 11:55:43 -0600

    Update - Added Performace Metric

--- src/app/Service/page-tracking.service.ts
+++ src/app/Service/page-tracking.service.ts
@@ -1,12 +1,14 @@
 import { Injectable, inject } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { Analytics, logEvent } from '@angular/fire/analytics';
+import { Performance, trace } from '@angular/fire/performance';
 import { filter } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PageTrackingService {
+  private perf = inject(Performance);
   private analytics = inject(Analytics);
   private router = inject(Router);
 
@@ -19,7 +21,14 @@ export class PageTrackingService {
           page_location: window.location.href,
           page_title: document.title,
         });
-        console.log(`📊 page_view logged for ${event.urlAfterRedirects}`);
+
+        // Performance trace
+        const t = trace(
+          this.perf,
+          `page_view_${event.urlAfterRedirects.replace(/\//g, '_')}`
+        );
+        t.start();
+        setTimeout(() => t.stop(), 1000); // optional stop after 1s
       });
   }
 }
